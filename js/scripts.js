345edc396b553e442c20ac414a54209ed8a642b3
Stephan Khoury <stephane.khoury@gmail.com>
2016-10-19 14:04:33 -0700

    add ALL OF THE HTML, add all the CSS styling AS WELL AS the working scripts for javavavavavavaaaa

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var personnameInput = $("input#personname").val();
+    var dateInput = $("input#date").val();
+    var starttimeInput = $("input#starttime").val();
+    var endtimeInput = $("input#endtime").val();
 
 
     $(".personname").text(personnameInput);
+    $(".date").text(dateInput);
+    $(".starttime").text(starttimeInput);
+    $(".endtime").text(endtimeInput);
 
 
     $("#bookinginfo").show();
+    alert("Thank you for scheduling your appointment with us, your requested time has been saved. Have a great day!");
 
 
     event.preventDefault();
